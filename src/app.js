2a95a3dcdb14f7c6a8eee08d77b1f69a9074151e
Anica <anicagabriellips@gmail.com>
2025-04-09 09:01:08 -0300

    Método get

--- src/app.js
+++ src/app.js
@@ -1,18 +1,30 @@
 // importando o express 
-const express = require('express');
+import express from 'express';
+
 // criando instância
 const app = express();
-// definindo a porta
-const port = 4000;
+
+// ,ock
+
+const selecoes = [
+    {id: 1, selecao: "Brasil", grupo: 'g'},
+    {id: 2, selecao: "Suíca", grupo: 'g'},
+    {id: 3, selecao: "Sérvia", grupo: 'g'},
+    {id: 4, selecao: "Eua", grupo: 'g'},
+    {id: 5, selecao: "Várxa", grupo: 'g'}
+]
+   
+
 
 // criar rota padrão ou raiz
 
+// ENDPOINT : parte final da url ex. /selecoes ou /alunos
 app.get('/', (req, res)=>{
-    res.send('careca é legal'+"piroca games");
+    res.send('careca é legal');
 })
 
-// escutar a porta
-
-app.listen(port, ()=>{
-    console.log(`Servidor rodando no endereço http://localhost:${port}`)
+app.get('/selecoes', (req, res)=>{
+    res.status(200).send(selecoes);
 })
+// exportando por padrão
+export default app;
